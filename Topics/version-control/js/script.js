82d1891ca9984f89adc0f1c189dd6eef278e22b2
pentonfreak <antonmcmilan@gmail.com>
2025-09-02 16:42:18 -0400

    Red dot in Yellow Box

--- Topics/version-control/js/script.js
+++ Topics/version-control/js/script.js
@@ -14,7 +14,7 @@
 */
 function setup() {
     // A 640x480 canvas
-    createCanvas(320, 320);
+    createCanvas(640, 640);
 
     // Don't show the cursor
     noCursor();
@@ -26,7 +26,7 @@ function setup() {
 */
 function draw() {
     // Make the background black (specified as RGB)
-    background(0, 2, 0);
+    background(0, 0, 0);
 
     // Draw a pyramid
     // How many levels for the pyramid
@@ -49,7 +49,7 @@ function draw() {
         // (320, 320) with a size based on the level
         // e.g. level 1 will be a 48x48 rectangle and
         // level 10 will be a 480x480 rectangle
-        rect(320, 320, level * 50, level * 50);
+        rect(320, 320, level * 48, level * 48);
         pop();
     }
 
