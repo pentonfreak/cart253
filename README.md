5e9f9c0dda5693e6c7b015c952284d99d554e446
pentonfreak <antonmcmilan@gmail.com>
2025-09-02 16:56:41 -0400

    README changed

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-fhfhgh
+First day doing this and it is cool
--- Topics/version-control/js/script.js
+++ Topics/version-control/js/script.js
@@ -26,7 +26,7 @@ function setup() {
 */
 function draw() {
     // Make the background black (specified as RGB)
-    background(1, 1, 1);
+    background(0, 0, 0);
 
     // Draw a pyramid
     // How many levels for the pyramid
