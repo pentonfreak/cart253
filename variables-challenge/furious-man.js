0fb5e0741a4dd4804797c3fc5e265f6a5d7fc17f
pentonfreak <antonmcmilan@gmail.com>
2025-09-16 16:36:16 -0400

    Update furious-man.js

--- variables-challenge/furious-man.js
+++ variables-challenge/furious-man.js
@@ -8,6 +8,8 @@
 "use strict";
 
 // Our friend Mr. Furious
+const rate = 2;
+
 let mrFurious = {
   // Position and size
   x: 200,
@@ -21,22 +23,48 @@ let mrFurious = {
   }
 };
 
+// The sky
+let sky = {
+  r: 100,
+  g: 150,
+  b: 255
+}; 
+
+// A bird
+let bird = {
+  x: 0,
+  y: 100,
+  size: 50,
+  speed: 2
+};
+
 /**
  * Create the canvas
  */
 function setup() {
   createCanvas(400, 400);
 }
+// Set the frame rate
+frameRate(rate);
+
+// Position the bird
+bird.x = 50;
+bird.y = 50;
+
+// Move the bird
+bird.x = bird.x + bird.speed;
 
 /**
  * Draw (and update) Mr. Furious
  */
 function draw() {
-  background(160, 180, 200);
+  background(sky.r, sky.g, sky.b);
+
   
   // Draw Mr. Furious as a coloured circle
   push();
-  noStroke();
+  stroke(0);
+  strokeWeight(2);
   fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
   ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
   pop();
