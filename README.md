4bfcc426aaf16315204f2386a951bd41ece13fdf
pentonfreak <antonmcmilan@gmail.com>
2025-09-23 16:15:31 -0400

    conditionals challenge finished

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 First day doing this and it is cool
 
 
-16/09/2025, it has been
+16/09/2025 - 
+
+23/09/2025 - Learned about conditionals of js.
--- conditionals-challenge/template-p5-project/js/script.js
+++ conditionals-challenge/template-p5-project/js/script.js
@@ -1,24 +1,99 @@
-/**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
- */
+/*
+//Circle Master
+//Khuong Nguyen
+//This will be a program in which the user can push a circle
+//on the canvas using their own circle.*/
 
-"use strict";
+const puck = {
+    x: 200,
+    y: 200,
+    size: 100,
+    fill: "#ff0000"
+};
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
+const user = {
+    x: undefined, // will be mouseX
+    y: undefined, // will be mouseY
+    size: 75,
+    fill: "#000000"
+};
+
+ 
+//Create the canvas*/
 function setup() {
-
+    createCanvas(400, 400);
 }
 
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ 
+//Move the user circle, check for overlap, draw the two circles*/
 function draw() {
+    background("#aaaaaa");
 
+    // Move user circle
+    moveUser();
+    movePuck();
+
+    // Draw the user and puck
+    drawUser();
+    drawPuck();
+}
+
+ 
+//Sets the user position to the mouse position*/
+function moveUser() {
+    user.x = mouseX;
+    user.y = mouseY;
+}
+
+function movePuck() {
+    const d = dist(user.x, user.y, puck.x, puck.y);
+
+    // Check if that distance is smaller than their two radii, 
+    // because if it is, they are overlapping by the amazing
+    // power of geometry!
+    const overlap = (d < user.size / 2 + puck.size / 2);
+
+    // Set fill based on whether they overlap
+    if (overlap) {
+        console.log("overlap!")
+        if (puck.x > user.x) {
+            console.log("yes")
+            puck.x += 1
+        }
+
+        if (puck.x < user.x) {
+            puck.x -= 1
+
+        }
+
+        if (puck.y > user.y) {
+            console.log("yes")
+            puck.y += 1
+        }
+
+        if (puck.y < user.y) {
+            puck.y -= 1
+
+        }
+    }
+}
+
+ 
+//Displays the user circle*/
+function drawUser() {
+    push();
+    noStroke();
+    fill(user.fill);
+    ellipse(user.x, user.y, user.size);
+    pop();
+}
+
+ 
+//Displays the puck circle*/
+function drawPuck() {
+    push();
+    noStroke();
+    fill(puck.fill);
+    ellipse(puck.x, puck.y, puck.size);
+    pop();
 }
